0c4ec1caac081a72a0dc703b3fa2fe31186738ba
Sean Parsons <seanparsons@Seans-MacBook-Pro-2.local>
2017-11-28 20:04:56 -0500

    actionable steps

--- game_table.py
+++ game_table.py
@@ -7,10 +7,46 @@ def Table():
 	def __init__(self):
 
 		self.player1 = Player('player1')
-		self.player2 = Player('player2')
 		self.dealer = Player('dealer')
 
-		self.playerList = [self.player1, self.player2, self.dealer]
+
+		self.playerList = [self.player1, self.dealer]
+
+		self.deck1 = Deck()
+
+
+
+
+
+
+		#########iterate through playerlist and draw cards#########
+
+
+
+
+
+
+
+		###########Add up points using calculate method from individual_player
+
+
+
+
+
+
+
+		#########Step or iterate function that takes a table, points of both player and dealer, actions of both, and rewards for both
+
+
+
+
+
+
+
+
+
+
+
 
 		
 
