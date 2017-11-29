2454b4820717028d5643110dabff9c6daad21ba6
nathan <nhjohnson10@gmail.com>
2017-11-28 20:33:02 -0500

    updated game_table to be playable by a computer

--- game_table.py
+++ game_table.py
@@ -9,45 +9,27 @@ def Table():
 		self.player1 = Player('player1')
 		self.dealer = Player('dealer')
 
-
 		self.playerList = [self.player1, self.dealer]
 
-		self.deck1 = Deck()
+		self.deck = Deck()
 
+	# iterate through playerList and draw cards
+	def dealHand(self):
+		for player in self.playerList:
+			player.draw(self.deck)
+			player.draw(self.deck)
 
+	# Add up points using calculate method from individual_player
+	def getPlayerScore(self):
+		return self.player1.addUpTotal()
 
+	def getDealerScore(self):
+		return self.dealer.addUpTotal()
 
+	# Step or iterate function that takes a table, points of both player and dealer, actions of both, and rewards for both
+	def hitPlayer(self):
+		self.player1.draw(self.deck)
 
-
-		#########iterate through playerlist and draw cards#########
-
-
-
-
-
-
-
-		###########Add up points using calculate method from individual_player
-
-
-
-
-
-
-
-		#########Step or iterate function that takes a table, points of both player and dealer, actions of both, and rewards for both
-
-
-
-
-
-
-
-
-
-
-
-
-		
-
-		
+	def playDealer(self):
+		while getDealerScore() < 17:
+			self.dealer.draw(self.deck)
